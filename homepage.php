9013790e8769ce0935f131145e29891ac5aeb418
JosephBannon <87155060+JosephBannon@users.noreply.github.com>
2022-11-23 20:24:52 -0500

    added navbar and changes some styling

--- homepage.php
+++ homepage.php
@@ -4,6 +4,8 @@ require("post-db.php");
 require("like-db.php");
 include("session.php");
 
+include("base.php");
+
 $list_of_posts = getAllPosts();
 $list_of_courses = getAllCourses();
 $list_of_likes = getAllLikes($_SESSION["login_ID"]);
@@ -31,8 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 }
 ?>
 
+
+
+
 <!DOCTYPE html>
 <html>
+
   
 <head>
   <meta charset="UTF-8">  
@@ -41,21 +47,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   <meta name="description" content="include some description about your page">      
   <title>Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="icon" type="image/png" href="http://www.cs.virginia.edu/~up3f/cs4750/images/db-icon.png" />
   <link rel="stylesheet" href=
 "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css">
 <link rel="stylesheet" href="mystyle.css">
-</link>
+
+
 </head>
 
-<body>
+
+
 <div class="container">
   <div class="row">
     <div class="col-8">
       <h1>Posts Feed</h1>  
-    </div>
-    <div class="col-4 text-end">
-      <a href="/databases/profile.php">My profile</a> 
     </div>
     <hr/>
   </div>
@@ -110,7 +114,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 <br>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
+<script> document.getElementById("homepage").classList.add('active'); </script>
 
 </body>
 </html>
